6d73c28b9eef08861e55b72d1387431092c42f58
Kiryl Parfiankou <Kirill.Parfenkov@gmail.com>
2016-05-17 21:34:37 +0300

    Initialization of pages-service

---
+++ pages-service/src/main/java/by/martin/eden/Application.java
@@ -0,0 +1,15 @@
+package by.martin.eden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Kiryl Parfiankou on 17.5.16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ pages-service/src/main/java/by/martin/eden/Greeting.java
@@ -0,0 +1,22 @@
+package by.martin.eden;
+
+/**
+ * Created by Kiryl Parfiankou on 17.5.16.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
